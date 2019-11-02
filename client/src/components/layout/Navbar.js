4cb0d9b17bce538002d587419de75050cdd92f8b
VicMontanez <v_montanez@hotmail.com>
2019-11-01 21:25:07 -0400

    changed navbar color

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
   render() {
@@ -15,6 +17,16 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <li className="nav-item">
+            <Link className="nav-link" to="/feed">
+              Post Feed
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
           <a
             href=""
             onClick={this.onLogoutClick.bind(this)}
@@ -49,10 +61,10 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-warning mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            Junto
+            Frindle
           </Link>
           <button
             className="navbar-toggler"
@@ -91,5 +103,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
